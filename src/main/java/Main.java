21b071b424eaf21ab6b54e1597249c7fc23728e1
BelkaLu <levikina.ea@gmail.com>
2023-09-10 19:23:48 +0300

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        // CalcMonth service = new CalcMonth
+        //int Calc.Month = service.calculate(100_000, 60_000, 150_000);
+        //System.out.println(RestMonth);
+    }
+}
